b6ebd58015f01dcb382ef4dbcf4e026f31a6d7ce
RPB <sumitomo.andi@gmail.com>
2023-10-06 19:07:32 +0700

    feat: add middlewares

--- dist/routes/task.route.js
+++ dist/routes/task.route.js
@@ -9,6 +9,7 @@ const cors_2 = __importDefault(require("../middlewares/cors"));
 const task_controller_1 = require("../controllers/task.controller");
 const taskRoutes = express_1.default.Router();
 taskRoutes.get('/v1/tasks', (0, cors_1.default)(cors_2.default.limitedClient), task_controller_1.getAllTask);
+taskRoutes.get('/v1/tasks/:id', (0, cors_1.default)(cors_2.default.limitedClient), task_controller_1.getOneTask);
 taskRoutes.post('/v1/tasks', (0, cors_1.default)(cors_2.default.limitedClient), task_controller_1.createTask);
 taskRoutes.options('/v1/tasks', (0, cors_1.default)(cors_2.default.limitedClient));
 taskRoutes.options('/v1/tasks', (0, cors_1.default)(cors_2.default.globalClient));
--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -5,6 +5,7 @@ import { getAllTask, getOneTask, createTask, updateTask, deleteTask } from '../c
 
 const taskRoutes = express.Router()
 taskRoutes.get('/v1/tasks',cors(clientAccess.limitedClient), getAllTask)
+taskRoutes.get('/v1/tasks/:id',cors(clientAccess.limitedClient), getOneTask)
 taskRoutes.post('/v1/tasks', cors(clientAccess.limitedClient), createTask)
 taskRoutes.options('/v1/tasks', cors(clientAccess.limitedClient))
 taskRoutes.options('/v1/tasks', cors(clientAccess.globalClient))
