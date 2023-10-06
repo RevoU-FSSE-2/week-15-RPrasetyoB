7f345112feeb21ecd19359cb8dcc44ede1cee0ea
RPB <sumitomo.andi@gmail.com>
2023-10-06 18:47:48 +0700

    feat: add middlewares

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,11 +1,11 @@
 const clientAccess = {
     limitedClient : {
-        origin: 'https://week15-rpb-x.netlify.app',
+        origin: ['https://week15-rpb-x.netlify.app', 'https://week15-rpb-x.netlify.app/*'],
         // origin: ['http://localhost:5173', 'http://localhost:5173/add', 'https://week15-rpb-x.netlify.app/'],
         methods: ['GET', 'POST']
     },
     globalClient : {
-        origin: 'https://week15-rpb-y.netlify.app',
+        origin: ['https://week15-rpb-y.netlify.app', 'https://week15-rpb-y.netlify.app/*'],
         methods: ['GET', 'POST', 'PATCH', 'DELETE']
     }
 }
--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -7,6 +7,7 @@ const taskRoutes = express.Router()
 taskRoutes.get('/v1/tasks',cors(clientAccess.limitedClient), getAllTask)
 taskRoutes.post('/v1/tasks', cors(clientAccess.limitedClient), createTask)
 taskRoutes.options('/v1/tasks', cors(clientAccess.limitedClient))
+taskRoutes.options('/v1/tasks', cors(clientAccess.globalClient))
 taskRoutes.patch('/v1/tasks/:id', cors(clientAccess.globalClient), updateTask)
 taskRoutes.delete('/v1/tasks/:id', cors(clientAccess.globalClient), deleteTask)
 
